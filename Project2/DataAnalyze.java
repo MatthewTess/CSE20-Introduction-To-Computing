066daaeb501dda0ea8afd23f97acdc5fce71a6b7
Matt <95320458+MatthewTess@users.noreply.github.com>
2023-07-03 04:14:49 -0700

    Update DataAnalyze.java

--- Project2/DataAnalyze.java
+++ Project2/DataAnalyze.java
@@ -1,108 +1,59 @@
-import java.util.Arrays.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class DataAnalyze {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Please enter the sample size: ");
+        int sample = input.nextInt();
 
-		System.out.print("Please enter the sample size: ");
-		int sample = input.nextInt();
+        int[][] trials = new int[4][sample];
 
-		System.out.println("Enter numbers for trial 0 ");
-		int[] trial= new int[sample];
-		for (int counter = 0;counter<trial.length;counter++) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Enter numbers for trial " + i);
+            for (int j = 0; j < sample; j++) {
+                System.out.print("Enter sample #" + j + ": ");
+                trials[i][j] = input.nextInt();
+            }
+        }
 
-			System.out.print("Enter sample #" + counter + ": ");
-			trial[counter] = input.nextInt();
-		}	
+        int[] totals = new int[4];
+        int[] averages = new int[4];
 
+        for (int i = 0; i < 4; i++) {
+            for (int element : trials[i]) {
+                totals[i] += element;
+            }
+            averages[i] = totals[i] / sample;
+        }
 
-		System.out.println("Enter numbers for trial 1 ");
-		int[] trial1= new int[sample];
-		for (int counter = 0;counter<trial1.length;counter++) {
+        System.out.println("\tSample #\tTrial 1\tTrial 2\tTrial 3\tTrial 4");
 
-			System.out.print("Enter sample #" + counter + ": ");
-			trial1[counter] = input.nextInt();
-		}	
+        for (int i = 0; i < sample; i++) {
+            System.out.print("\t\t" + i + "\t\t");
+            for (int j = 0; j < 4; j++) {
+                System.out.print(trials[j][i] + "\t\t");
+            }
+            System.out.println();
+        }
 
-		System.out.println("Enter numbers for trial 2 ");
-		int[] trial2= new int[sample];
-		for (int counter = 0;counter<trial2.length;counter++) {
+        Arrays.sort(averages);
+        int maxAvg = averages[3];
+        int minAvg = averages[0];
 
-			System.out.print("Enter sample #" + counter + ": ");
-			trial2[counter] = input.nextInt();
-		}	
+        System.out.println("------------------------------------------------------------");
+        System.out.println("      Average :\t" + averages[0] + "\t\t" + averages[1] + "\t\t" + averages[2] + "\t\t" + averages[3]);
+        System.out.println("Min Average: " + minAvg);
+        System.out.println("Max Average: " + maxAvg);
 
-		System.out.println("Enter numbers for trial 3 ");
-		int[] trial3= new int[sample];
-		for (int counter = 0;counter<trial3.length;counter++) {
-
-			System.out.print("Enter sample #" + counter + ": ");
-			trial3[counter] = input.nextInt();
-
-
-
-
-
-		}	
-		int total = 0;
-		int total1= 0;
-		int total2=0;
-		int total3 = 0;
-
-		for (int element : trial) {
-			total += element;
-
-		}
-
-		for (int element : trial1) {
-			total1 += element;
-
-		}
-
-		for (int element : trial2) {
-			total2 += element;
-
-		}
-
-		for (int element : trial3) {
-			total3 += element;
-
-		}
-
-		int average = total/sample;
-		int average1 = total1/sample;
-		int average2 = total2/sample;
-		int average3 = total3/sample;
-
-		System.out.println("\tSample #\tTrial 1\tTrial 2\tTrial 3\tTrial 4");
-
-		for (int i = 0; i<trial1.length; i++) {
-			System.out.println( "         " +  i + "                " + trial[i] + " " + "      " + trial1[i] + " " + "      " + trial2[i] + " " + "      " + trial3[i] );
-		}	
-
-		int[]averages= {average, average1, average2, average3};
-		Arrays.sort(averages);
-		int maxavg = averages[averages.length - 1];
-		int minavg = averages[0];
-		for(int i=0; i<trial.length; i++){
-
-			System.out.println("------------------------------------------------------------");
-			System.out.println("      Average :" + "           " + average + "       " + average1 + "       " + average2 + "       " + average3);
-			System.out.println("Min Average: " + minavg);
-			System.out.println("Max Average: " +  maxavg);
-
-		}
-		if(maxavg == minavg) {
-			System.out.println("the trials match EXACTLY!");
-		}else if(maxavg < 2*minavg) {
-			System.out.println("The trials concur with each other!");
-		}else {
-			System.out.println("The trials do NOT concur!");
-		}
-	}
+        if (maxAvg == minAvg) {
+            System.out.println("The trials match EXACTLY!");
+        } else if (maxAvg < 2 * minAvg) {
+            System.out.println("The trials concur with each other!");
+        } else {
+            System.out.println("The trials do NOT concur!");
+        }
+    }
 }
